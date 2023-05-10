df687a7ae01b183d1d1f447fe9bff56bab5f3099
Gustavo Koichi <guga_koichi@hotmail.com>
2023-05-10 18:39:53 -0300

    Consulta SQL, Projection, Buscar games por lista

--- src/main/java/com/gustavokoichi/dslist/dto/GameMinDTO.java
+++ src/main/java/com/gustavokoichi/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.gustavokoichi.dslist.dto;
 
 import com.gustavokoichi.dslist.entities.Game;
+import com.gustavokoichi.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -21,6 +22,13 @@ public class GameMinDTO {
 		shortDescription = entity.getShortDescription();
 	}
 
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
+	}
 	public Long getId() {
 		return id;
 	}
